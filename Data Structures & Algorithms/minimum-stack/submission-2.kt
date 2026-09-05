5211813daecff499725afe848ce13d0201add4c0
Julio Lemus <jclemus91@users.noreply.github.com>
2026-09-05 14:07:08 -0600

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.kt
@@ -0,0 +1,28 @@
+class MinStack() {
+    
+    private val stack = ArrayDeque<Int>()
+    private val minStack = ArrayDeque<Int>()
+
+    fun push(`val`: Int) {
+        stack.addLast(`val`)
+        
+        if (minStack.isEmpty() || `val` < minStack.last()) {
+            minStack.addLast(`val`)
+        } else {
+            minStack.addLast(minStack.last())
+        }
+    }
+
+    fun pop() {
+        stack.removeLast()
+        minStack.removeLast()
+    }
+
+    fun top(): Int {
+        return stack.last()
+    }
+
+    fun getMin(): Int {
+        return minStack.last()
+    }
+}
